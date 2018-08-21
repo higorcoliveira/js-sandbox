3b7f6d5fb87952759af5c54a24999c089729e554
higoroliveira <higor.oliveira@afferolab.com.br>
2018-08-21 18:26:55 -0300

    types and functions

---
+++ typescript/functions.ts
@@ -0,0 +1,13 @@
+function add(a: number, b: number) {
+    return a + b;
+}
+
+let result = add(1, 2);
+console.log(result);
+
+function addVarargs(a, b, c?) { // optional argument, can be assign a value as well (c = 0)
+    return a + b + c;
+}
+
+let result2 = addVarargs(1, 2, 3);
+console.log(result2);
---
+++ typescript/types.ts
@@ -0,0 +1,17 @@
+let a: number = 10;
+let b: string = "teste";
+let c: boolean = true;
+
+console.log(a, b, c);
+
+let myArray: number[]; // array declaration
+myArray = [];
+myArray = [1, 2];
+myArray.push(1);
+
+console.log(myArray);
+
+let arrayWithDifferentTypes: [number, boolean]; // tuple
+arrayWithDifferentTypes = [1, true];
+
+console.log(arrayWithDifferentTypes);
